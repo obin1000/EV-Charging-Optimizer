0e7929dc1730b834690b9f8ff99ba84d86b94284
Robin Vonk <git@robinvonk.com>
2023-10-09 17:19:49 +0200

    Added

--- arrive_cars.m
+++ arrive_cars.m
@@ -1,9 +1,16 @@
 function cars = arrive_cars(t, average, capacity)
     % Returns a list of new arriving cars
+    % Results are based on time of day and average provided
     cars = [];
+
+    % Calculate the fraction of the day represented by the input time
+    fractionOfDay = t / 1440;  % 1440 minutes in a day
+    
+    % Map the fraction to the desired range (0 to 1)
+    time_scaler = 1.5 - abs(fractionOfDay - 0.5) * 2;
+    
     % Use exponential distribution for arriving cars
-    % TODO: Do this based on the time (t)
-    for car=1:round(exprnd(average, 1, 1))
+    for car=1:round(time_scaler*exprnd(average, 1, 1))
         % Calculate a random charge for the car
         car_charge = min(exprnd(0.2)*capacity, capacity);
         cars = [cars, Car(capacity, car_charge, t)];
