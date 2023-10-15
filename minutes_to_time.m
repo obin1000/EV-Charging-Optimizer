033c37968142ab60598fbd33455bee80d2232548
Robin Vonk <git@robinvonk.com>
2023-10-15 11:53:45 +0200

    Added car layout

--- minutes_to_time.m
+++ minutes_to_time.m
@@ -1,6 +1,6 @@
-function [hours,minutes] = minutes_to_time(minutes)
+function [hours,minute] = minutes_to_time(minutes)
 %MINUTES_TO_TIME Converts a total sum of minutes to a normal clock format
 hours = floor(minutes / 60);
-minutes = mod(minutes, 60);
+minute = mod(minutes, 60);
 end
 
